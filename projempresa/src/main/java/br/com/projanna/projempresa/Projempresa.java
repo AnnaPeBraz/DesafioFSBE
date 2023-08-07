b83a1443f938e1bfd78e1828a204d06458f7f152
AnnaPeBraz <annap.0103@gmail.com>
2023-08-07 18:38:23 -0300

    criando a classe empresa e tentando com Sout

--- projempresa/src/main/java/br/com/projanna/projempresa/Projempresa.java
+++ projempresa/src/main/java/br/com/projanna/projempresa/Projempresa.java
@@ -11,6 +11,9 @@ package br.com.projanna.projempresa;
 public class Projempresa {
 
     public static void main(String[] args) {
+        Empresa falatu = new Empresa(1, "123456", "falatu", 123456);
+        
         System.out.println("Hello World!");
+        System.out.println(falatu.getId());
     }
 }
